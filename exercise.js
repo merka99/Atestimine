2eb9e2c8f781bb1fce0257a12dad024bfaa95df4
merka99 <merle.pakko@khk.ee>
2019-03-13 14:32:46 +0200

    uus

--- exercise.js
+++ exercise.js
@@ -1,11 +1,14 @@
-function add(x, y) {
-    var result;
-    if (typeof x && typeof y) !== 'number') {
-        throw new Error('Number');
-    }
-    result = x+y;
-    if parseInt(result) !== result) {
-        result = parseFloat(result.toFixed(1));
-    }
-    return result;
+//function add(x, y) {
+  //  var result;
+    //if (typeof x && typeof y) !== 'number') {
+    //    throw new Error('Number');
+//    }
+  //  result = x+y;
+//    if parseInt(result) !== result) {
+  //      result = parseFloat(result.toFixed(1));
+//    }
+  //  return result;
 }
+expect(add(2,3)).toBe(5);
+expect(add(2, 'test')).toThrow();
+expect(add(0.1, 0.2)).toBe(0.3);
