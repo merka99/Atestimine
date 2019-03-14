6d6500209bfea8a0c32ef6dc451155d22c931f27
Merle Päkko <merle.pakko@khk.ee>
2019-03-14 09:49:28 +0200

    muudatused

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,38 @@
+describe('Address Book', function () {
+    let addressBook,
+        thisContact;
+
+    beforeEach(function(){
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
+    
+     it('should be able to add a contact', function () {
+        let addressBook = new AddressBook(),
+         thisContact = new Contact();
+
+     addressBook.addContact(thisContact);
+     expect(addressBook.getContact(0)).toBe(thisContact);
+
+    });
+    it('should be able to delete a contact', function(){
+        var addressBook = new AddressBook(),
+            thisContact = new Contact();
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+        
+        expect(addressBook.getContact(0)), not.toBeDefined();
+    }); 
+describe('Async Address Book', function () {
+        let addressBook = new AddressBook();
+
+      beforeEach(function(done){
+      addressBook.getInitialContacts(function () {
+         done();
+        });
+      });
+    it('should grab initial contacts', function(done){
+        except(AddressBook.initialComplete).toBe(true);
+        done();
+    });
+});
